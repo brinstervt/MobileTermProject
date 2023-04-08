8ec6bbd359693d5be42d3cb97ffad231a13000ee
zaccomer40 <zaccomer40@gmail.com>
2023-04-07 20:52:55 -0400

    Added Example Cards to the View

---
+++ app/src/main/java/com/example/termproject/BookItem.kt
@@ -0,0 +1,9 @@
+package com.example.termproject
+
+data class BookItem (
+    val title:String,
+    val author:String,
+    val publicationDate:String,
+    val rating:Float,
+    val imageUrl:String = "",
+    )
