a87b588c6d0b6d3da73c4337427eddc8c44501ed
zaccomer40 <zaccomer40@gmail.com>
2023-04-28 12:17:51 -0400

    logout button

--- app/src/main/java/com/example/termproject/SettingsFragment.kt
+++ app/src/main/java/com/example/termproject/SettingsFragment.kt
@@ -1,11 +1,19 @@
 package com.example.termproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 class SettingsFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +26,15 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, bundle: Bundle?) {
         super.onViewCreated(view, bundle)
 
+
+        auth = Firebase.auth
+
+        view.findViewById<Button>(R.id.logout_button).setOnClickListener {
+            auth.signOut()
+            val intent = Intent(context, LoginActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
 
     }
 }
