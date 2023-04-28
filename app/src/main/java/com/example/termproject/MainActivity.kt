fcf404396910c6321ca2e8db225819cddbd522cf
zaccomer40 <zaccomer40@gmail.com>
2023-04-28 12:13:38 -0400

    added settings page

--- app/src/main/java/com/example/termproject/MainActivity.kt
+++ app/src/main/java/com/example/termproject/MainActivity.kt
@@ -10,18 +10,27 @@ import android.widget.AutoCompleteTextView
 import android.widget.RatingBar
 import android.widget.SearchView
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.os.bundleOf
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.termproject.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     private lateinit var searchView: SearchView
     private lateinit var exposedDropdownMenu: TextInputLayout
     private lateinit var bookList: RecyclerView
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/termproject/SettingsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.termproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, bundle: Bundle?) {
+        super.onViewCreated(view, bundle)
+
+
+    }
+}
